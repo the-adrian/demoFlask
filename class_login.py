6ed2f5f455a6ef11e768c8584dc6cbd2eefbeaef
Jesús Adrián Ramírez López <jadrianramirezl@gmail.com>
2014-06-04 16:38:18 -0500

    Se agregaron la validacion

--- class_login.py
+++ class_login.py
@@ -4,12 +4,18 @@ from flask import request
 class Login(flask.views.MethodView):
     def post(self):
         usuario = None
-        error = None
+        password =None
+        message = None
         if request.method == 'POST':
             usuario = request.form['usuario']
-        else:
-            error = 'basura'
-        return flask.render_template('login.html', usuario=usuario, error=error)
+            password = request.form['password']
+            if usuario == 'Adrian' and password == '123':
+                message = 'Welcome'
+                return flask.render_template('login.html', message = message)
+            else:
+                message = 'Access denied'
+                return flask.render_template('login.html',message = message)
+
 
     def get(self):
         usuario = None
