b1cf0d229082c3be0821d2f52b5aa2e6c26e6f16
root <root@localhost.localdomain>
2014-06-04 16:44:31 -0500

    metodo acomodado

--- class_login.py
+++ class_login.py
@@ -12,10 +12,4 @@ class Login(flask.views.MethodView):
         return flask.render_template('login.html', usuario=usuario, error=error)
 
     def get(self):
-        usuario = None
-        error = None
-        if request.method == 'POST':
-            usuario = request.form['usuario']
-        else:
-            error = 'basura'
-        return flask.render_template('login.html', usuario=usuario, error=error)
+       return flask.render_template('login.html')
