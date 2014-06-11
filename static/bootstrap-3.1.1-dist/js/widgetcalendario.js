75edf046b2ff4a165fb53c17141cd7508bf865b5
cleon <cleon@localhost.localdomain>
2014-06-11 12:20:50 -0500

    Agregando librerias para el calendario

---
+++ static/bootstrap-3.1.1-dist/js/widgetcalendario.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+$('.Wcalendar').datetimepicker({
+    language:  'es',
+    weekStart: 1,
+    todayBtn:  1,
+	autoclose: 1,
+	todayHighlight: 1,
+	startView: 2,
+	forceParse: 0,
+    showMeridian: 1
+});
+});
