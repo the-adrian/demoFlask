77e75d8464fb17760ce02ae027ead19bdf65e986
Jesús Adrián Ramírez López <jadrianramirezl@gmail.com>
2014-06-18 09:38:11 -0500

    Agregado la consulta por fecha y hora

--- class_db.py
+++ class_db.py
@@ -30,7 +30,8 @@ def validar_usuario(query):
 def consultar_ventas(dateStart, dateEnd):
     dateStart = libgral.separateDate(dateStart)
     dateEnd = libgral.separateDate(dateEnd)
-
+    # 2014/06/13 11:46:25
+    # 2014/06/13 14:05:37
     query = "SELECT No_Serie, Turno, Fecha, Hora, Ticket, Tarifa, Total FROM Tabla_Ventas " \
             "WHERE Fecha BETWEEN STR_TO_DATE('"+dateStart[0]+"', '%Y/%m/%d') AND STR_TO_DATE('"+dateEnd[0]+"','%Y/%m/%d')" \
             "AND Hora BETWEEN TIME_FORMAT('"+dateStart[1]+"','%T') AND TIME_FORMAT('"+dateEnd[1]+"','%T');"
