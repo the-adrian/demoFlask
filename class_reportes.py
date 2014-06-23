c5f6425de5e13b67ffe44d79b64af89137274aaf
Jesús Adrián Ramírez López <jadrianramirezl@gmail.com>
2014-06-23 16:31:40 -0500

    Tabla generadad desde el controlador

--- class_reportes.py
+++ class_reportes.py
@@ -1,21 +1,26 @@
 #-*- coding: utf-8 -*-
 from flask import render_template, redirect,request
 import flask
-from web.contrib.template import render_cheetah
 import class_db
 
+num_col = 7
 class Reportes(flask.views.MethodView):
     def post(self):
         fechaInicio = request.form['fechaInicio']
         fechaFin = request.form['fechaFin']
         tabla_ventas = class_db.consultar_ventas(fechaInicio, fechaFin)
+        codigo_tabla = ""
         if tabla_ventas == None:
             msg = True
             return render_template('home.html', msg = msg)
         else:
-            #for venta in tabla_ventas:
+            for venta in tabla_ventas:
+                codigo_tabla += str('<tr class="active">')
+                for indice in range(num_col):
+                    codigo_tabla += str("<td>")+ str(tabla_ventas[indice + 1]) + str("</td>")
 
-            return render_template('home.html',tabla_ventas = tabla_ventas, fechaFin = fechaFin, fechaInicio = fechaInicio)
+                str("</tr>")
+            return render_template('home.html',codigo_tabla = codigo_tabla, fechaFin = fechaFin, fechaInicio = fechaInicio)
     def get(self):
         return render_template('home.html')
 
